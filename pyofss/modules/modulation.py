c3bec7df1b97b0ee1668afb9be354861b6245c3e
Denis Sergeevich <galilley@gmail.com>
2022-04-13 11:50:31 +0700

    Modulation: add docstring

--- pyofss/modules/modulation.py
+++ pyofss/modules/modulation.py
@@ -23,7 +23,26 @@ from pyofss.field import temporal_power
 
 
 class Modulation(object):
-    "SAM"
+    """
+    The module realize a parabolic point action self-amplitude modulation
+    (SAM) transmission function expressed as
+
+        rho(P) = rho_max - (P/P_cr - 1)^2 * (rho_max - rho_min),
+
+    which is a quite good approximation for NPE-based artifisial saturable 
+    absorber [1,2].
+
+    param string name: Name of this module
+    param double Pcr: Critical power [W]
+    param double rho_max: Maximal transmission coefficient [a.u.] 
+                            (rho_min < rho_max < 1)
+    param double rho_min: Minimal transmission at P=0 [a.u.]
+                            (0 < rho_min < rho_max)
+
+    [1] A. E. Bednyakova et al., Opt. Express 21(18), 20556 (2013).
+    [2] A. E. Bednyakova et al., JOSA B 37(9), 2763 (2020).
+
+    """
 
     def __init__(self, name="modulation",
                  Pcr=None, rho_max=None, rho_min=None):
@@ -35,6 +54,6 @@ class Modulation(object):
 
     def __call__(self, domain, field):
         P = temporal_power(field)
-        self.g = self.rho_max - (P/self.Pcr - 1.)**2*(self.rho_max - self.rho_min)
+        self.g = self.rho_max - ((P/self.Pcr - 1.)**2) * (self.rho_max - self.rho_min)
         self.g = np.where(self.g < self.rho_min, self.rho_min, self.g)
         return field * self.g
